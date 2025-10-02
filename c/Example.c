640c3c655d5f7a8fe0b7d3d85bc8ec3e931f5457
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-10-03 01:11:09 +0530

    Update

---
+++ c/Example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int x;
+	printf("WHAT THE FUCK DO U WANT TO BE X : ");
+	scanf("&d",&x);
+	int power = x^3, &x;
+	printf("&d", power);
+	return 0;
+}
